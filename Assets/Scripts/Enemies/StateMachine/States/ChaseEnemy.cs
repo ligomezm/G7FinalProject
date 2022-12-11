c67c09ac4cd07c2071f14d0a4451df7634bda921
maucamsan <47923785+maucamsan@users.noreply.github.com>
2022-12-11 11:16:57 -0500

    Set AI enemies basic behavior
    Three basic states were set using the statemachinebehavior script from
    unity. Idle, chase and wander. Next, an attack behaviour will be added.
    Also set the scene to have a navmesh and the corresponing prefabs to
    accomplish this behavior.

---
+++ Assets/Scripts/Enemies/StateMachine/States/ChaseEnemy.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseEnemy : StateMachineBehaviour
+{
+    public float repathTolerance = 2;
+    public float repathCount = 10;
+    public float runSpeed = 4;
+    public static float chaseRadius = 10;
+
+    PlayerSingleton target;
+    
+    NavMeshAgent navAgent;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) 
+    {
+        if (navAgent == null)
+            navAgent = animator.GetComponent<NavMeshAgent>();
+        if (target == null)
+            target = PlayerSingleton.GetInstance();
+
+        navAgent.ResetPath();
+        navAgent.speed = runSpeed;
+    }
+
+    public override void OnStateMove(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) 
+    {
+        if (!ShouldChasePlayer(animator.transform.position))
+            animator.SetInteger(IdleEnemy.transitionParameter, (int) Transition.IDLE);
+        else 
+            if (!navAgent.hasPath || (target.transform.position - navAgent.pathEndPosition).sqrMagnitude > repathTolerance * repathTolerance)
+                SetDestinationNearTarget(navAgent, target);    
+    }
+
+    public void SetDestinationNearTarget(NavMeshAgent _agent, PlayerSingleton _target)
+    {
+        NavMeshHit hit;
+        float radius = 0;
+        for (int i = 0; i < repathCount; ++i)
+        {
+            Vector3 randomPosition = Random.insideUnitSphere * radius;
+            randomPosition += _target.transform.position;
+            if(NavMesh.SamplePosition(randomPosition, out hit, radius, 1))
+            {
+                _agent.SetDestination(hit.position);
+                break;
+            }
+            else
+                ++radius;
+        }
+    }
+    public static bool ShouldChasePlayer(Vector3 _chaserPosition)
+    {
+        PlayerSingleton p = PlayerSingleton.GetInstance();
+        return (p.transform.position - _chaserPosition).sqrMagnitude < chaseRadius * chaseRadius;
+    }
+}
