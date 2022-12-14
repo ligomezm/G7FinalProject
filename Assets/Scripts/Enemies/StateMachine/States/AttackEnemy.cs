b245775b5caa0e455d1b74341687ae593bb49528
maucamsan <47923785+maucamsan@users.noreply.github.com>
2022-12-14 10:59:39 -0500

    Create Attack enemy behavior and modify Chase enemy to change to new state

--- Assets/Scripts/Enemies/StateMachine/States/AttackEnemy.cs
+++ Assets/Scripts/Enemies/StateMachine/States/AttackEnemy.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class AttackEnemy : MonoBehaviour
+public class AttackEnemy : StateMachineBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    NavMeshAgent navAgent;
+    PlayerSingleton target;
+    public static float minAttackDistance = 2;
+    public float walkSpeed = 0;
+    public float fovAngle = 60;
+    public float fovDistance = 10; // To review
+    internal Vector3 lastKnownPosition = Vector3.zero;
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) 
     {
-        
+        if (navAgent == null)
+            navAgent = animator.GetComponent<NavMeshAgent>();
+        if (target == null)
+            target = PlayerSingleton.GetInstance();
+
+        navAgent.ResetPath();
+        // navAgent.speed = walkSpeed;
+        navAgent.isStopped = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) 
     {
-        
+        if (!ShouldAttackPlayer(animator.transform.position))
+            animator.SetInteger(IdleEnemy.transitionParameter, (int) Transition.CHASE);
+        Debug.Log("Attacking");
+        // Add attack functionality
     }
+
+    public static bool ShouldAttackPlayer(Vector3 attackerPosition)
+    {
+        PlayerSingleton player = PlayerSingleton.GetInstance();
+
+        return (attackerPosition - player.transform.position).sqrMagnitude <= minAttackDistance * minAttackDistance;
+    }
+
+    
 }
