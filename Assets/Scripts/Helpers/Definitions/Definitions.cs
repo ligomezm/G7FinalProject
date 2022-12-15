184e39414b76b46f79a9338c3c1f90067c08d788
maucamsan <47923785+maucamsan@users.noreply.github.com>
2022-12-15 10:57:38 -0500

    Set the canvas manager scripts

--- Assets/Scripts/Helpers/Definitions/Definitions.cs
+++ Assets/Scripts/Helpers/Definitions/Definitions.cs
@@ -7,6 +7,11 @@ public enum Transition
 {
     IDLE, WANDER, CHASE, ATTACK, DEATH
 }
+
+public enum CanvasType
+{
+    BOOT, MAINMENU, GAMEUI, MUSEUM, PAUSE, GAMEOVER, VICTORY 
+}
 public class Definitions : MonoBehaviour
 {
    
