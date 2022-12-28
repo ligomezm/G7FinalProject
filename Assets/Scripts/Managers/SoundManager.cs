921cac7a5de7f37ed5fda263538268d81963fbe3
maucamsan <47923785+maucamsan@users.noreply.github.com>
2022-12-28 18:32:42 -0500

    Adjust canvas prefab and set some lines on the sound manager script

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -9,4 +9,9 @@ public class SoundManager : Singleton<SoundManager>
     {
         effectSource.PlayOneShot(clip);
     }
+
+    public void ChangeMasterVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
 }
