8db5a9744fc160c833014735644c4e317abb1f84
BAYRON DAVID MORALES <bayron.david.morales@gmail.com>
2023-01-12 20:35:45 -0500

    Particles system in attack animations

--- Assets/Scripts/Attack/Attack.cs
+++ Assets/Scripts/Attack/Attack.cs
@@ -1,5 +1,7 @@
+using CurlNoiseParticleSystem.Emitter;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
@@ -8,21 +10,19 @@ public class Attack : MonoBehaviour
     List<BoxCollider> colliderWeapon = new List<BoxCollider>();
     Animator animator;
 
-    int countClicks;
-    bool canClick;
-
+    private List<DistanceEmitter> _emitter = new List<DistanceEmitter>();
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        countClicks = 0;
-        canClick = false;
 
 
-        for(int i = 0; i < weapons.transform.childCount; i++)
+
+        for (int i = 0; i < weapons.transform.childCount; i++)
         {
-            colliderWeapon.Add(weapons.transform.GetChild(i).gameObject.GetComponent<BoxCollider>());    
+            colliderWeapon.Add(weapons.transform.GetChild(i).gameObject.GetComponent<BoxCollider>());
+                  _emitter.Add(weapons.transform.GetChild(i).gameObject.GetComponent<DistanceEmitter>());
         }
         DesactiveColliders();
     }
@@ -37,7 +37,14 @@ public class Attack : MonoBehaviour
     {
         for(int j = 0; j < colliderWeapon.Count; j++)
         {
-            colliderWeapon[j].enabled = true;
+            try
+            {
+                _emitter[j].Play();
+                colliderWeapon[j].enabled = true;
+            }
+            catch (System.Exception)
+            {
+            }
         }
     }
 
@@ -45,7 +52,14 @@ public class Attack : MonoBehaviour
     {
         for (int j = 0; j < colliderWeapon.Count; j++)
         {
-            colliderWeapon[j].enabled = false;
+            try
+            {
+                _emitter[j].Stop();
+                colliderWeapon[j].enabled = false;
+            }
+            catch (System.Exception)
+            {
+            }
         }
     }
 
