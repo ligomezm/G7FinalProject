9b3afa6f35807a9abbfea697676058733c3302cf
BAYRON DAVID MORALES <bayron.david.morales@gmail.com>
2023-01-13 14:37:24 -0500

    modify sword damage

--- Assets/Scripts/Attack/SwordDamage.cs
+++ Assets/Scripts/Attack/SwordDamage.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class SwordDamage : MonoBehaviour
 {
+    public float swordDamage =  30;
+    public bool isSpecialAttack;
     
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,11 @@ public class SwordDamage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // If it is special Attack the sword damage increased 10% 
+        if (isSpecialAttack)
+        {
+            swordDamage = swordDamage + swordDamage * 0.1f;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,7 +29,17 @@ public class SwordDamage : MonoBehaviour
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             EnemyDamage enemyDamage = other.gameObject.GetComponent<EnemyDamage>();
-            enemyDamage.hp -= enemyDamage.swordDamage;
+            enemyDamage.hp -= swordDamage;
         }
+    }
+
+    public void ActiveSpecialAttack()
+    {
+        isSpecialAttack = true;
+    }
+
+    public void DesativeSpecialAttack()
+    {
+        isSpecialAttack = true;
     }
 }
