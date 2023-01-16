e60368a0d937946ed2cb4c24bb8062633742ceda
maucamsan <47923785+maucamsan@users.noreply.github.com>
2023-01-15 22:31:31 -0500

    Base empty script to create progression system

---
+++ Assets/Scripts/Managers/XpManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XpManager : Singleton<XpManager>
+{
+   public void IncreaseAttack()
+   {
+    
+   }
+}
