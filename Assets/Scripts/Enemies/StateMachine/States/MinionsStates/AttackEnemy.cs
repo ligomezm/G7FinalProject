05e0411f96d71879a61a84cf8f550fedf60db2ee
maucamsan <47923785+maucamsan@users.noreply.github.com>
2023-01-17 20:19:10 -0500

    Fix enemy behavior on attack
    Now the enemy completely culminates the attack animation before changing
    its state based on the player's position. Even if the player exits the
    detection range fot the attack trigger, the enemy will end its
    animation.

--- Assets/Scripts/Enemies/StateMachine/States/MinionsStates/AttackEnemy.cs
+++ Assets/Scripts/Enemies/StateMachine/States/MinionsStates/AttackEnemy.cs
@@ -7,12 +7,14 @@ public class AttackEnemy : StateMachineBehaviour
 {
     NavMeshAgent navAgent;
     PlayerSingleton target;
+    public float animationTime;
     public static float minAttackDistance = 2;
     public float attackDistance;
     public float walkSpeed = 0;
     public float fovAngle = 60;
     public float fovDistance = 10; // To review
     internal Vector3 lastKnownPosition = Vector3.zero;
+    internal bool isAttacking = false;
     public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) 
     {
         minAttackDistance = attackDistance;
@@ -28,8 +30,14 @@ public class AttackEnemy : StateMachineBehaviour
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) 
     {
-        if (!ShouldAttackPlayer(animator.transform.position))
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= animationTime)
+        {
+            isAttacking = false;
+        }
+        if (!ShouldAttackPlayer(animator.transform.position) && !isAttacking)
             animator.SetInteger(IdleEnemy.transitionParameter, (int) Transition.CHASE);
+        else if (ShouldAttackPlayer(animator.transform.position))
+            isAttacking = true;
         int data = animator.GetComponent<AIComponent>().enemyBehavior.BaseDamage;
         //Debug.Log($"Attacking with {data}");
         // animator.SetInteger(IdleEnemy.transitionParameter, (int) Transition.ATTACK);
@@ -40,7 +48,7 @@ public class AttackEnemy : StateMachineBehaviour
     public static bool ShouldAttackPlayer(Vector3 attackerPosition)
     {
         PlayerSingleton player = PlayerSingleton.GetInstance();
-
+        
         return (attackerPosition - player.transform.position).sqrMagnitude <= minAttackDistance * minAttackDistance;
     }
 
