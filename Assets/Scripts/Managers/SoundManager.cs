7ea8c1a3ca3320bc9a67c2a7e6207a7d02164a53
Liliana Gómez <83715511+ligomezm@users.noreply.github.com>
2023-01-24 17:26:07 -0500

    Sound Manager

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : Singleton<SoundManager>
 {
-    [SerializeField] private AudioSource musicSource, effectSource; 
+    [SerializeField] private AudioSource musicSource, effectSource;
+    string currentLevelName;
+
+    private void Start()
+    {
+        currentLevelName = SceneManager.GetActiveScene().ToString();
+    }
+
     public void PlaySound(AudioClip clip)
     {
         effectSource.PlayOneShot(clip);
     }
     public void PlayMusic(AudioClip musicClip)
     {
-        musicSource.Play();
+        if (currentLevelName == "Level2")
+            musicSource.Stop();
     }
     public void PlayClipAtPointRef(AudioClip clip, Vector3 clipTransform)
     {
