1f546e56dca271377a4e5548546332f548447d09
BAYRON DAVID MORALES <bayron.david.morales@gmail.com>
2023-01-24 20:08:40 -0500

    fix Animation Enemy

--- Assets/Scripts/Attack/EnemyDamage.cs
+++ Assets/Scripts/Attack/EnemyDamage.cs
@@ -43,8 +43,8 @@ public class EnemyDamage : MonoBehaviour
         if (hp <= 0 && flag== false)
         {
             flag = true;
+            gameObject.GetComponent<CapsuleCollider>().enabled = false;
             animator.SetInteger("State", 5);
-
             sound_Death.Play();
         }
         hp = Mathf.Clamp(hp, minValue, maxValue);
